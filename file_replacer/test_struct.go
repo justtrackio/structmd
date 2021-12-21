a99f2991a4603684ba612ec7bf0df29b16959cf5
Liviu <liviu@justdice.io>
2021-12-21 12:02:10 +0100

    made it parse time.Duration type;

--- file_replacer/test_struct.go
+++ file_replacer/test_struct.go
@@ -1,5 +1,7 @@
 package file_replacer
 
+import "time"
+
 // MySettingsStruct does smth
 // line 1
 // line two
@@ -16,4 +18,9 @@ type MyOtherStruct struct {
 	// Port does smth 11.
 	// Port related comment 2.
 	Port string `cfg:"port" default:"2222"`
+}
+
+type MyTimeStruct struct {
+	// Read timeout is the maximum duration for reading the entire request, including the body.
+	Read time.Duration `cfg:"read" default:"60s" validate:"min=1000000000"`
 }
